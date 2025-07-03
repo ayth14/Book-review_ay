3b8392dd64f9bd6f85e09e4473559ffe8f466ca0
ast68 <ayushthunder14@gmail.com>
2025-07-03 22:48:47 +0530

    Related task(s): filter for the date range filter seperate function in books.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -20,8 +20,39 @@ class Book extends Model
         return $this->hasMany(Reviews::class);
     }
 
-    public function scopeTitle(Builder $query, $title)
+    public function scopeTitle(Builder $query, string $title): Builder
     {
-        return $query->where("title", "like", "%" . $title . "%");
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews' =>
+            fn(Builder $q) =>
+            $this->dateRangeFilter($q, $from, $to)
+        ])
+            ->orderByDesc('reviews_count');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withAvg([
+            'reviews' =>
+            fn(Builder $q) =>
+            $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    private function dateRangeFilter(Builder $qry, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $qry->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $qry->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $qry->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
